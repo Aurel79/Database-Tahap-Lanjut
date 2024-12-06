0964888d6bccbfd45a809d8d3af5ea71847caecb
Aurel79 <faurelia795@gmail.com>
2024-12-06 19:02:29 +0700

    Database-Tahap-Lanjut

---
+++ laravel-controller-view/resources/views/default.blade.php
@@ -0,0 +1,4 @@
+<div class="d-flex align-items-center py-2 px-4 bg-light rounded-3 border">
+    <div class="bi-house-fill me-3 fs-1"></div>
+    <h4 class="mb-0">Well done! This is {{ $pageTitle }}.</h4>
+</div>
---
+++ laravel-controller-view/routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EmployeeController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// home kiri itu untuk akses url dan yang kanan untuk navbar yang ada di file home.blade.php/
+Route::get('home', [HomeController::class, 'index'])->name('home');
+Route::get('profile', ProfileController::class)->name('profile');
+Route::resource('employees', EmployeeController::class);
+Route::get('/employees/{employee}/edit', [EmployeeController::class, 'edit'])->name('employees.edit');
+Route::put('/employees/{employee}', [EmployeeController::class, 'update'])->name('employees.update');
+
